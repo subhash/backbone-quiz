668afc225d389d5a4db835a98433121c53698b76
Subhash Gopalakrishnan <subhashgo@gmail.com>
2013-01-15 09:50:02 +0530

    Back and next

--- js/view.js
+++ js/view.js
@@ -1,5 +1,5 @@
 (function($) {
-	
+
 	var Question = Backbone.Model.extend( {
 		defaults : {
 			coverImage : "img/placeholder.png",
@@ -14,11 +14,13 @@
 	var Quiz = Backbone.Collection.extend( {
 		model : Question
 	});
-	
+
 	var QuestionAnswerView = Backbone.View.extend( {
-		tagName: "div",
-		template: $("#questionAnswerTemplate").html(),
-		render: function(){
+		tagName : "div",
+		className : "questionContainer",
+		template : $("#questionAnswerTemplate").html(),
+
+		render : function() {
 			var tmpl = _.template(this.template);
 			this.$el.html(tmpl(this.model.toJSON()));
 			return this;
@@ -26,35 +28,72 @@
 	});
 
 	var QuizAnswerView = Backbone.View.extend( {
-		el: $("#quizAnswer"),
-		
-		initialize: function(){
+		el : $("#quizAnswer"),
+		controls : $("#questionAnswerControls").html(),
+
+		initialize : function() {
 			this.collection = new Quiz(quiz);
+			this.index = 0;
 			this.render();
 		},
-		
-		render: function(){
-			that = this;
-			_.each(this.collection.models, function(item) {
-				that.renderQuestion(item);
-			}, this);
+
+		render : function() {
+			this.renderQuestion(this.collection.models[this.index]);
+		},
+
+		renderQuestion : function(item) {
+			var questionAnswerView = new QuestionAnswerView( {
+				model : item
+			});
+			this.$el.html(questionAnswerView.render().el);
+			var tmpl = _.template(this.controls);
+			var param = {
+				"next" : (this.index < this.collection.models.length - 1),
+				"back" : (this.index > 0)
+			};
+			this.$el.append(tmpl(param));
 		},
 		
-		renderQuestion: function(item){
-			var questionAnswerView = new QuestionAnswerView({model: item});
-			console.log(questionAnswerView.render().el);
-			this.$el.append(questionAnswerView.render().el);
+		backQuestion: function() {
+			if(this.index > 0)
+				this.index = this.index - 1;
+			this.render();
+		},
+
+		nextQuestion : function() {
+			if(this.index < this.collection.models.length - 1)
+				this.index = this.index + 1;
+			this.render();
+		},
+
+		events : {
+			"click .next" : "nextQuestion",
+			"click .back" : "backQuestion"
 		}
-		
+
 	});
 
-	var quiz = [
-	            {question : "What's your favourite colour?",
-				choice1 : "Yellow",
-				choice2 : "Orange",
-				choice3 : "Red",
-				choice4 : "Pink"} 
-	            ]
+	var quiz = [ {
+		question : "What's your favourite colour?",
+		choice1 : "Yellow",
+		choice2 : "Orange",
+		choice3 : "Red",
+		choice4 : "Pink"
+	}, {
+		question : "How are you?",
+		choice1 : "Happy",
+		choice2 : "Sad",
+		choice3 : "Dull",
+		choice4 : "Bored"
+
+	}, {
+		question : "What's today?",
+		choice1 : "Monday",
+		choice2 : "Wednesday",
+		choice3 : "Sunday",
+		choice4 : "Friday"
+
+	} ]
 
 	var quizAnswerView = new QuizAnswerView();
 
